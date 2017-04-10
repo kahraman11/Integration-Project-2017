670f274b9bd5a2195a2be7243921abb32cf81d8e
Mathay Kahraman <g.m.kahraman@student.utwente.nl>
2017-04-10 12:23:53 +0200

    Add setConnection second try

--- src/PacketHandling/Network.java
+++ src/PacketHandling/Network.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 /**
  * Created by mathay on 10-4-17.
@@ -20,6 +22,20 @@ public class Network {
 
     public void setConnection() throws IOException {
         //get/set nodenr
+        Enumeration e = NetworkInterface.getNetworkInterfaces();
+        while(e.hasMoreElements())
+        {
+            NetworkInterface n = (NetworkInterface) e.nextElement();
+            Enumeration ee = n.getInetAddresses();
+            while (ee.hasMoreElements())
+            {
+                InetAddress iaddress = (InetAddress) ee.nextElement();
+                if (iaddress.toString().contains("192")) {
+                    nodenr = Integer.parseInt(iaddress.toString().substring(iaddress.toString().length() - 1));
+                }
+            }
+        }
+
         if (Inet4Address.getLocalHost().getHostAddress().contains("192")) {
             nodenr = Integer.parseInt(Inet4Address.getLocalHost().getHostAddress().substring(Inet4Address.getLocalHost().getHostAddress().toString().length() - 1));
             System.out.println("IP Address: " + Inet4Address.getLocalHost().getHostAddress() + " Nodenr: " + nodenr);
